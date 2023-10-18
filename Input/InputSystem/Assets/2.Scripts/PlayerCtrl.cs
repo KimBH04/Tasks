af7131aef4142162bc1242be830dab8b7675e8d2
KimBH04 <kimbh0437@gmail.com>
2023-10-18 11:29:49 +0900

    분노봇

--- Input/InputSystem/Assets/2.Scripts/PlayerCtrl.cs
+++ Input/InputSystem/Assets/2.Scripts/PlayerCtrl.cs
@@ -9,10 +9,41 @@ public class PlayerCtrl : MonoBehaviour
     private Transform tr;
     private Vector3 moveDir;
 
+    private PlayerInput pi;
+    private InputActionMap iam;
+    private InputAction movAction;
+    private InputAction atkAction;
+
     private void Start()
     {
         anime = GetComponent<Animator>();
         tr = GetComponent<Transform>();
+        pi = GetComponent<PlayerInput>();
+
+        iam = pi.actions.FindActionMap("PlayerActions");
+
+        movAction = iam.FindAction("Move");
+        atkAction = iam.FindAction("Attack");
+
+        movAction.performed += ctx =>
+        {
+            Vector2 dir = ctx.ReadValue<Vector2>();
+            moveDir = new Vector3(dir.x, 0, dir.y);
+
+            anime.SetFloat("Movement", dir.magnitude);
+        };
+
+        movAction.canceled += ctx =>
+        {
+            moveDir = Vector3.zero;
+            anime.SetFloat("Movement", 0f);
+        };
+
+        atkAction.performed += ctx =>
+        {
+            anime.SetTrigger("Attack");
+            Debug.Log("Attack");
+        };
     }
 
     private void Update()
@@ -24,12 +55,11 @@ public class PlayerCtrl : MonoBehaviour
         }
     }
 
+    #region SEND_MESSAGE
     private void OnMove(InputValue value)
     {
         Vector2 dir = value.Get<Vector2>();
-
         moveDir = new Vector3(dir.x, 0, dir.y);
-
         anime.SetFloat("Movement", dir.magnitude);
         Debug.Log($"Move = ({dir.x}, {dir.y})");
     }
@@ -39,4 +69,25 @@ public class PlayerCtrl : MonoBehaviour
         anime.SetTrigger("Attack");
         Debug.Log("Attack");
     }
+    #endregion
+
+    #region UNITY_EVENTS
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 dir = ctx.ReadValue<Vector2>();
+        moveDir = new Vector3(dir.x, 0, dir.y);
+        anime.SetFloat("Movement", dir.magnitude);
+    }
+
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log($"ctx.phase{ctx.phase}");
+
+        if (ctx.performed)
+        {
+            Debug.Log("Attack");
+            anime.SetTrigger("Attack");
+        }
+    }
+    #endregion
 }
