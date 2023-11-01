c6e41920d113fc05ad1e551fffb6be96833199f5
KimBH04 <kimbh0437@gmail.com>
2023-11-01 13:07:26 +0900

    포톤 매니저

--- Pothon/AngryBot2Net/Assets/1.Scripts/PhotonManager.cs
+++ Pothon/AngryBot2Net/Assets/1.Scripts/PhotonManager.cs
@@ -1,18 +1,63 @@
+using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhotonManager : MonoBehaviour
+public class PhotonManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
+    private readonly string version = "1.0";
+    private string userID = "KimBH";
+
+    private void Awake()
     {
-        
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = version;
+        PhotonNetwork.NickName = userID;
+
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        Debug.Log("Connected to master!");
+        Debug.Log($"PhotonNetwork.InLobby : {PhotonNetwork.InLobby}");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"JoinRandom Failed {returnCode} : {message}");
+
+        RoomOptions room = new RoomOptions();
+        room.MaxPlayers = 20;
+        room.IsOpen = true;
+        room.IsVisible = true;
+
+        PhotonNetwork.CreateRoom("My Room", room);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room");
+        Debug.Log($"Room Name = {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}");
+        Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}");
+
+        foreach (Player player in PhotonNetwork.CurrentRoom.Players.Values)
+        {
+            Debug.Log($"{player.NickName}, {player.ActorNumber}");
+        }
     }
 }
